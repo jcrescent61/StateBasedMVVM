757d4536e641206550e3d817b182584473880e46
Ellen J <jcrescent61@frientrip.com>
2022-11-29 16:55:56 +0900

    뭉탱이커밋

---
+++ StateBasedMVVM/StateBasedMVVM/Error.swift
@@ -0,0 +1,8 @@
+//
+//  Error.swift
+//  StateBasedMVVM
+//
+//  Created by Ellen J on 2022/11/29.
+//
+
+import Foundation
---
+++ StateBasedMVVM/StateBasedMVVM/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewMOdel.swift
+//  StateBasedMVVM
+//
+//  Created by Ellen J on 2022/11/29.
+//
+
+import Foundation
